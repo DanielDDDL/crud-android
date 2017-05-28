06d98739cc002bde12efc88bc65d065f29daa30b
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-05-28 20:32:53 -0300

    adding new_book_activity's buttons' actions

--- app/src/main/java/daniel/com/br/crud/NewBookActivity.java
+++ app/src/main/java/daniel/com/br/crud/NewBookActivity.java
@@ -1,20 +1,30 @@
 package daniel.com.br.crud;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import daniel.com.br.crud.database.BookDaoSQLite;
+import daniel.com.br.crud.model.Book;
 
 public class NewBookActivity extends AppCompatActivity {
 
     //widgets
     private EditText txtTitle, txtAuthor;
     private Button btnRegister, btnBack;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_book);
+
+        this.context = this;
 
         //getting widgets from view
         txtTitle = (EditText)findViewById(R.id.txtTitle);
@@ -22,5 +32,63 @@ public class NewBookActivity extends AppCompatActivity {
         btnRegister = (Button)findViewById(R.id.btnRegister);
         btnBack = (Button)findViewById(R.id.btnBack);
 
+        //buttons click events
+        btnRegister.setOnClickListener(new BtnRegisterOnClickListener());
+        btnBack.setOnClickListener(new BtnBackOnClickListener());
+
     }
+
+    private class BtnRegisterOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+
+            if (areFieldsValid()){
+                //get book's information
+                String title = txtTitle.getText().toString();
+                String author = txtAuthor.getText().toString();
+                Book book = new Book(title,author); //setting values
+
+                //register book on database
+                new BookDaoSQLite(context).insertBook(book);
+
+                //show message
+                String messageToast = title + " was sucessfully register";
+                Toast.makeText(context,messageToast,Toast.LENGTH_SHORT).show();
+
+                //clean fields
+                txtTitle.setText("");
+                txtAuthor.setText("");
+
+            }
+        }
+    }
+
+    private class BtnBackOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent newActivityMain = new Intent(context,MainActivity.class);
+            startActivity(newActivityMain);
+        }
+    }
+
+    //return true if all the fields text are valid
+    //also it sets an error message on the txts invalid, so that the user knows
+    private boolean areFieldsValid(){
+        boolean isValid = true;
+        //title
+        if (txtTitle.getText().toString().isEmpty()){
+            isValid = false;
+            txtTitle.setError("Title's field must be filled");
+        }
+        //author
+        if (txtAuthor.getText().toString().isEmpty()){
+            isValid = false;
+            txtAuthor.setError("Author's field must be filled");
+        }
+
+        return isValid;
+    }
+
 }
