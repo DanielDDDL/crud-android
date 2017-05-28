040cf6d1b275abbebaa928fb63b675ded3fa1ee4
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-05-28 19:35:43 -0300

    adding list view, adapter and their layout item

--- app/src/main/java/daniel/com/br/crud/MainActivity.java
+++ app/src/main/java/daniel/com/br/crud/MainActivity.java
@@ -1,19 +1,35 @@
 package daniel.com.br.crud;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listBooks;
+
+    //test
+    private ArrayAdapter<String>testAdapter;
+    private static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
+    private Context testContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //tests porpuses
-        Intent newActivityIntent = new Intent(this,UpdateOrDeleteActivity.class);
-        startActivity(newActivityIntent);
+        this.testContext = this;
+        listBooks = (ListView)findViewById(R.id.listBooks);
+        testAdapter = new ArrayAdapter<String>(this,R.layout.simple_list_item,FRUITS);
+        listBooks.setAdapter(testAdapter);
+//        //tests porpuses
+//        Intent newActivityIntent = new Intent(this,NewBookActivity.class);
+//        startActivity(newActivityIntent);
 
     }
 }
