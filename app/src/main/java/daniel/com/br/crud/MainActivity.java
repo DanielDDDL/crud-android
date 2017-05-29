0ab101763a7501105e31a83ffda08bd4b0926bef
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-05-28 21:32:18 -0300

    adding adapter to list view

--- app/src/main/java/daniel/com/br/crud/MainActivity.java
+++ app/src/main/java/daniel/com/br/crud/MainActivity.java
@@ -4,34 +4,72 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import daniel.com.br.crud.database.BookDaoSQLite;
+import daniel.com.br.crud.model.Book;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listBooks;
+    //widgets
+    private ListView lvBooks;
+    private Button btnNewBook;
 
-    //test
-    private ArrayAdapter<String>testAdapter;
-    private static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
-            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
-            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
-    private Context testContext;
+    //adapter dealing with the books
+    private ArrayAdapter<Book> adapter;
+
+    //list of books objects
+    private List<Book> listBooks;
+
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.context = this;
 
+        //getting all the books from the database
+        //adding them to the current working book list
+        listBooks = new BookDaoSQLite(context).selectBooks();
 
+        //loading all the books on the list onto the list view
+        adapter = new ArrayAdapter<Book>(context,R.layout.simple_list_item,listBooks);
+        lvBooks = (ListView)findViewById(R.id.lvBooks);
+        lvBooks.setAdapter(adapter);
+
+        //add new book button
+        Button btnNewBook = (Button)findViewById(R.id.btnNewBook);
+        btnNewBook.setOnClickListener(new BtnNewBookListener());
+
+//        testes
 //        this.testContext = this;
 //        listBooks = (ListView)findViewById(R.id.listBooks);
 //        testAdapter = new ArrayAdapter<String>(this,R.layout.simple_list_item,FRUITS);
 //        listBooks.setAdapter(testAdapter);
-//        //tests porpuses
-        Intent newActivityIntent = new Intent(this,NewBookActivity.class);
-        startActivity(newActivityIntent);
+//        Intent newActivityIntent = new Intent(MainActivity.this,UpdateOrDeleteActivity.class);
+//        newActivityIntent.putExtra("title","Titulo");
+//        newActivityIntent.putExtra("author","Autor");
+//        newActivityIntent.putExtra("id",10);
+//        startActivity(newActivityIntent);
 
     }
+
+    private class BtnNewBookListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(context,NewBookActivity.class);
+            context.startActivity(intent);
+        }
+    }
+
 }
