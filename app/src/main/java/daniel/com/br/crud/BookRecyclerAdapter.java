6db7b715edc754468e2b466643ec0fa29bc79159
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-06-11 18:39:34 -0300

    editing operation

--- app/src/main/java/daniel/com/br/crud/BookRecyclerAdapter.java
+++ app/src/main/java/daniel/com/br/crud/BookRecyclerAdapter.java
@@ -1,8 +1,13 @@
 package daniel.com.br.crud;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,6 +18,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import daniel.com.br.crud.database.BookDaoSQLite;
 import daniel.com.br.crud.model.Book;
 
 /**
@@ -23,11 +29,17 @@ public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapte
 
     private Context mContext;
     private List<Book> bookList;
+    private RecyclerView recyclerView;
 
     public BookRecyclerAdapter(Context mContext, List<Book> bookList){
         this.mContext = mContext;
         this.bookList = bookList;
 
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
     }
 
     @Override
@@ -41,7 +53,7 @@ public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapte
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
 
-        Book clickedBook = bookList.get(position);
+        final Book clickedBook = bookList.get(position);
         holder.txtTitle.setText(clickedBook.getTitle());
         holder.txtAuthor.setText(clickedBook.getAuthor());
 
@@ -49,40 +61,64 @@ public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapte
         holder.ivOverflow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showPopupMenu(holder.ivOverflow);
+                showPopupMenu(holder.ivOverflow, clickedBook);
             }
         });
 
     }
 
-    private void showPopupMenu (View view){
+    private void showPopupMenu (View view, final Book clickedBook){
         //inflating menu
         PopupMenu popup = new PopupMenu(mContext, view);
         MenuInflater inflater = popup.getMenuInflater();
         inflater.inflate(R.menu.book_menu,popup.getMenu());
-        popup.setOnMenuItemClickListener(new PopupMenuItemClickListener());
-        popup.show();
-    }
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_edit:
+                        Intent intent = new Intent(mContext,UpdateOrDeleteActivity.class);
+                        intent.putExtra("id",clickedBook.getId());
+                        intent.putExtra("title",clickedBook.getTitle());
+                        intent.putExtra("author",clickedBook.getAuthor());
+                        intent.putExtra("genre",clickedBook.getGenre());
 
-    class PopupMenuItemClickListener implements PopupMenu.OnMenuItemClickListener{
+                        mContext.startActivity(intent);
+                        return true;
+                    case R.id.action_delete:
 
-        //TODO: what is this boolean return for?
+                        //ask for the user to confirm that he wants to delete the book
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(mContext);
+                        alertDialogBuilder.setTitle("Deleting book")
+                            .setMessage("Are you sure you really wanna do this?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //confirming willing to delete book...
+                                    new DeleteBookLoader().execute(clickedBook.getId());
+                                    //TODO: reload data
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener(){
 
-        @Override
-        public boolean onMenuItemClick(MenuItem item) {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //cancelling operation
+                                    dialog.cancel();
+                                }
+                            });
 
-            switch (item.getItemId()){
-                case R.id.action_edit:
-                    //TODO add here the edit action
-                    return true;
-                case R.id.action_delete:
-                    //TODO add here the delete action
-                    return true;
-                default:
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+                        alertDialog.show();
+
+                        return true;
+                    default:
+                }
+
+                return false;
             }
-
-            return false;
-        }
+        });
+        popup.show();
     }
 
     @Override
@@ -107,5 +143,44 @@ public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapte
         }
     }
 
+    class DeleteBookLoader extends AsyncTask<Integer,Void,Integer>{
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            Integer id = params[0];
+            new BookDaoSQLite(mContext).delete(id);
+
+            return findBookByIdOnBookList(id);
+        }
+
+        @Override
+        protected void onPostExecute(Integer index) {
+            super.onPostExecute(index);
+            if (index != -1){
+                //delete from the list as well
+                //and notifying changes
+                bookList.remove(index);
+                notifyDataSetChanged();
+//                recyclerView.setAdapter(new BookRecyclerAdapter(mContext,bookList));
+//                recyclerView.invalidate();
+//                recyclerView.removeViewAt(index);
+//                notifyItemRemoved(index);
+//                notifyItemRangeChanged(index,bookList.size());
+
+            }
+
+        }
+    }
+
+    private int findBookByIdOnBookList(int id){
+        for (int i = 0; i < bookList.size(); i++){
+            if (bookList.get(0).getId() == id){
+                return i;
+            }
+        }
+        //not found
+        return -1;
+
+    }
 
 }
